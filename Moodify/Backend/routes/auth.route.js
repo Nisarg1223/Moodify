22d95e41341ccca514bbf47cbef1d9fdcdae4dba
Nisarg1223 <darjinisarg49@gmail.com>
2026-03-13 21:09:23 +0530

    Removed .env from repository

--- Moodify/Backend/routes/auth.route.js
+++ Moodify/Backend/routes/auth.route.js
@@ -1,9 +1,15 @@
-const express = require('express');
+const express = require("express");
 const authRouter = express.Router();
-const authController = require('../controllers/auth.controller.js')
-const authmiddleware = require('../middlewares/auth.middleware.js');
-authRouter.post('/register',authController.registerUser);
-authRouter.post('/login',authController.loginUser);
-authRouter.get('/get-me',authmiddleware.authUser,authController.getMe);
-authRouter.get('/logout',authController.logout)
-module.exports = authRouter
+
+const authController = require("../controllers/auth.controller.js");
+const authmiddleware = require("../middlewares/auth.middleware.js");
+
+authRouter.post("/register", authController.registerUser);
+
+authRouter.post("/login", authController.loginUser);
+
+authRouter.get("/get-me", authmiddleware.authUser, authController.getMe);
+
+authRouter.get("/logout", authController.logout);
+
+module.exports = authRouter;
