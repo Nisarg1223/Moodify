f6477d0f28fa3998b971ef79c76480039a56d947
Nisarg1223 <darjinisarg49@gmail.com>
2026-05-05 21:08:23 +0530

    fix: update CORS configuration to allow multiple origins

--- Moodify/Backend/src/app.js
+++ Moodify/Backend/src/app.js
@@ -8,7 +8,10 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin:'moodify-black-pi.vercel.app',
+     origin: [
+        'http://localhost:5173',
+        'https://moodify-black-pi.vercel.app'
+    ],
     
     credentials:true
 }));
